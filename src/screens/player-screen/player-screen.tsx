6fb414c22ecdf62323affb05f223a07bc2b0df35
Stup <meadow174@gmail.com>
2023-12-20 11:11:16 +0500

    fix for pass check

--- src/screens/player-screen/player-screen.tsx
+++ src/screens/player-screen/player-screen.tsx
@@ -86,17 +86,19 @@ export function PlayerScreen() {
 
         <div className="player__controls-row">
           <button type="button" className="player__play" onClick={onPlayPauseClick}>
-            {isPause ? <>
-              <svg viewBox="0 0 19 19" width="19" height="19">
-                <use xlinkHref="#play-s"/>
-              </svg>
-              <span>Play</span>
-            </> : <>
-              <svg viewBox="0 0 14 21" width="14" height="21">
-                <use xlinkHref="#pause"/>
-              </svg>
-              <span>Pause</span>
-            </>}
+            {isPause ?
+              <>
+                <svg viewBox="0 0 19 19" width="19" height="19">
+                  <use xlinkHref="#play-s"/>
+                </svg>
+                <span>Play</span>
+              </> :
+              <>
+                <svg viewBox="0 0 14 21" width="14" height="21">
+                  <use xlinkHref="#pause"/>
+                </svg>
+                <span>Pause</span>
+              </>}
           </button>
           <div className="player__name">Transpotting</div>
 
